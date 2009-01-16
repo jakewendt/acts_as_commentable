e5cb76c03aa41088de0aff240f1f1be8f6569f37
Jake Wendt <github@jake.otherinbox.com>
2009-01-15 21:37:42 -0700

    Make functioning tests.

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
 # Include hook code here
+$:.unshift "#{File.dirname(__FILE__)}/lib"    # needed for testing
 require 'acts_as_commentable'
-
+require 'comment'
+ActiveRecord::Base.send(:include, Juixe::Acts::Commentable)
--- test/acts_as_commentable_test.rb
+++ test/acts_as_commentable_test.rb
@@ -1,8 +1,61 @@
 require 'test/unit'
+require 'rubygems'
+require 'active_record'
+require 'active_support/test_case'
+require File.dirname(__FILE__) + '/../init'
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memory:")
+
+def setup_db
+	ActiveRecord::Schema.define(:version => 1) do
+		create_table :comments do |t|
+			t.text :body
+			t.references :commentable, :polymorphic => true
+			t.timestamps
+		end
+		create_table :posts do |t|
+			t.string :title
+			t.text   :body
+			t.timestamps
+		end
+	end
+end
+
+def teardown_db
+	ActiveRecord::Base.connection.tables.each do |table|
+		ActiveRecord::Base.connection.drop_table(table)
+	end
+end
+
+class Post < ActiveRecord::Base
+	acts_as_commentable
+end
 
 class ActsAsCommentableTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
-  end
+
+	def setup
+		setup_db
+	end
+
+	def teardown
+		teardown_db
+	end
+
+	def test_the_truth
+		assert true
+	end
+
+	def test_post
+		assert_difference('Post.count', 1, "1 Post should've been created") {
+			@post = Post.create!({:title => "My Title", :body => "My Post Body"})
+		}
+		assert_difference('Post.first.comments.count', 1, "1 Comment should've been created") {
+		assert_difference('Comment.count', 1, "1 Comment should've been created") {
+			@post.add_comment(Comment.create!({:body => "My Comment"}))
+		} }
+
+		@comments = Post.find_comments_for(@post)
+		assert @comments.length == 1
+	end
+
 end
